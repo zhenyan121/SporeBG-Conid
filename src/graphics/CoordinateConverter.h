9d8d2a010c8e71d4f6bd1a81ab693927913a3a52
zhenyan121 <104683324+zhenyan121@users.noreply.github.com>
2025-12-06 18:37:53 +0800

    Add CoordinateConverter to get logical postion

---
+++ src/graphics/CoordinateConverter.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "utils/Config.h"
+#include <SDL3/SDL.h>
+#include <utility>
+#include <optional>
+class CoordinateConverter {
+public:
+    CoordinateConverter(SDL_Renderer* renderer);
+    ~CoordinateConverter();
+    // 将物理坐标转化成逻辑坐标
+    std::optional<std::pair<int, int>> ScreenToBoard(float screenX, float screenY, BoardArea aera);
+
+private:
+    SDL_Renderer* m_renderer;
+};
