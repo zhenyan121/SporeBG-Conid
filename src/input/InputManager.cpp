995c01bd8ca68db43158fdf3e7088e9b2045bea7
zhenyan121 <3367366583@qq.com>
2025-12-06 18:38:36 +0800

    Remove the game logic from InputManager

--- src/input/InputManager.cpp
+++ src/input/InputManager.cpp
@@ -1,6 +1,6 @@
 #include "InputManager.h"
 
-SDL_AppResult InputManager::handleInputEvent(const SDL_Event* event, const Renderer* g_renderer, Game* g_game) {
+SDL_AppResult InputManager::handleInputEvent(const SDL_Event* event) {
 
     switch (event->type) {
         // 如果检测到退出
@@ -8,46 +8,16 @@ SDL_AppResult InputManager::handleInputEvent(const SDL_Event* event, const Rende
             return SDL_APP_SUCCESS;
         case SDL_EVENT_MOUSE_BUTTON_DOWN:
             if (event->button.button == SDL_BUTTON_LEFT) {
-                // 将窗口坐标转为逻辑坐标
-                float logicalX, logicalY;
-                SDL_RenderCoordinatesFromWindow(
-                    g_renderer->getSDLRenderer(),
-                    static_cast<float>(event->button.x),
-                    static_cast<float>(event->button.y),
-                    &logicalX,
-                    &logicalY
-                );
-                auto boardArea = g_renderer->getBoardArea();
-                auto click = handleMouseClick(static_cast<int>(logicalX), static_cast<int>(logicalY), boardArea);
-                if (click) {
-                    auto [row, col] = click.value();
-                    SDL_Log("click on (%d, %d)", row, col);
-                    g_game->handleCoordinateInput(row, col);
-                    g_game->printBoard();
-                } else {
-                    SDL_Log("invail cilck aera!");
-                }
+                // 更新状态
+                m_currentInputState.mouseCilckOn = {event->button.x, event->button.y};
+                
+                
                 return SDL_APP_CONTINUE;
             }
     }
     return SDL_APP_CONTINUE;
 }
 
-std::optional<std::pair<int, int>> 
-InputManager::handleMouseClick(int mouseX, int mouseY, const ui::BoardArea& area) {
-    // 判断是否点击在棋盘区域内
-    if (mouseX < area.x || mouseX >= area.x + area.cellSize * area.cols ||
-        mouseY < area.y || mouseY >= area.y + area.cellSize * area.rows) {
-        return std::nullopt; // 点击在棋盘外
-    }
-
-    // 转换为逻辑坐标
-    int col = (mouseX - area.x) / area.cellSize;
-    int row = (mouseY - area.y) / area.cellSize;
-
-    // 安全检查（通常不需要，但保险）
-    if (row >= 0 && row < area.rows && col >= 0 && col < area.cols) {
-        return std::pair<int, int>{row, col};
-    }
-    return std::nullopt;
+InputState InputManager::GetInputState() const {
+    return m_currentInputState;
 }
