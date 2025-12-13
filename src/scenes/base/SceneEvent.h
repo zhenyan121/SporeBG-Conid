6f5c378cb0d8be19462750aca38efa4952646155
zhenyan121 <3367366583@qq.com>
2025-12-13 16:07:22 +0800

    Added SceneEvent callback

---
+++ src/scenes/base/SceneEvent.h
@@ -0,0 +1,26 @@
+/**
+ * @file    SceneEvent.h
+ * @brief   定义场景事件类型和结构体
+ * @author  zhenyan121
+ * @date    2025-12-13
+ */
+#pragma once
+#include <string>
+/**
+ * @enum    SceneEventType
+ * @brief   定义场景事件的类型
+ */
+enum class SceneEventType {
+    ChangeScene,    ///< 切换场景事件
+    PushScene,   ///< 推入新场景事件
+    PopScene,   ///< 弹出当前场景事件
+    QuitGame    ///< 退出游戏事件
+};
+/**
+ * @struct  SceneEvent
+ * @brief   场景事件结构体，包含事件类型和相关数据
+ */
+struct SceneEvent {
+    SceneEventType type;    ///< 事件类型
+    std::string sceneName;  ///< 只在 ChangeScene/PushScene 时使用
+};
---
+++ src/ui/base/IUIManager.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include "ui/components/Button.h"
+#include "ui/components/Label.h"
+#include "ui/base/UIRenderData.h"
+
+#include <memory>
+#include <vector>
+#include <unordered_map>
+
+class IUIManager {
+public:
+    virtual ~IUIManager() = default;
+
+    virtual void init() = 0;
+
+    virtual const UIRenderData& getUIRenderData() = 0;
+    // 收集渲染数据
+    virtual void CollectRenderData() = 0;
+
+    virtual void UpdateMousePositon(float x, float y) = 0;
+
+protected:
+    std::unordered_map<int, std::unique_ptr<Button>> m_buttons;
+    std::unordered_map<int, std::unique_ptr<Label>> m_labels;
+    UIRenderData m_uiRenderData;
+    SDL_Renderer* m_renderer = nullptr;
+    TextRenderer* m_textRenderer = nullptr;
+
+    size_t makeHash(const std::string& name) {
+        return std::hash<std::string>{}(name);
+    }
+};
