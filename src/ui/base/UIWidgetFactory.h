473666f95c3369346b8bfe7254a9a2debe95ac57
zhenyan121 <3367366583@qq.com>
2025-12-22 13:32:55 +0800

    change logictexture to 640x360

---
+++ src/ui/base/UIWidgetFactory.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "ui/components/Button.h"
+#include "ui/components/Label.h"
+class UIWidgetFactory {
+public:
+
+    static std::unique_ptr<Button> createStandardButton(const std::string& name, const std::string& text, int x, int y, std::function<void()> callback) {
+        auto button = std::make_unique<Button>();
+        button->setBackgroundColor({0, 150, 255, 255});
+        button->setBorder(2, {0, 0, 0, 255});
+        button->setRect(x, y, UI::ButtonSize * 6, UI::ButtonSize * 4);
+        button->setName(name);
+        button->setText(text, {"SourceHanSansSC-Regular.otf", UI::UI_NORMAL_FONT_SIZE, {255, 255, 255, 255}});
+        button->setCallback(callback);
+        return button;
+    }
+
+    static std::unique_ptr<Label> createStandardLabel(const std::string& name, const std::string& text, int x, int y) {
+        auto label = std::make_unique<Label>();
+        label->setRect(x, y, UI::ButtonSize * 4, UI::ButtonSize * 2);
+        label->setText(text, {"SourceHanSansSC-Regular.otf", UI::UI_NORMAL_FONT_SIZE, {0, 0, 0, 255}});
+        label->setName(name);
+        return label;
+    }
+
+};
