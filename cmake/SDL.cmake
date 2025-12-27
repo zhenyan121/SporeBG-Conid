2f421ec7a8222bcd3f289b43b812a6abe931f102
zhenyan121 <104683324+zhenyan121@users.noreply.github.com>
2025-12-27 20:12:08 +0800

    updated SDL3 version

--- cmake/SDL.cmake
+++ cmake/SDL.cmake
@@ -6,7 +6,7 @@ include(FetchContent)
 FetchContent_Declare(
     SDL3
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-    GIT_TAG release-3.2.4
+    GIT_TAG release-3.2.28
 )
 
 FetchContent_MakeAvailable(SDL3)
