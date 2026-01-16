777a9678f5a1b98c611646305992124393264b20
zhenyan121 <104683324+zhenyan121@users.noreply.github.com>
2026-01-16 17:56:41 +0800

    perf: optimize texture caching

--- src/graphics/texture/TextureManager.cpp
+++ src/graphics/texture/TextureManager.cpp
@@ -24,10 +24,10 @@ void TextureManager::cleanupAllTexture() {
 
 
 
-SDL_Texture* TextureManager::createTextureFromRect(int x, int y, SDL_FRect& rect, SDL_Color& color) {
+SDL_Texture* TextureManager::createTextureFromRect(SDL_FRect& rect, SDL_Color& color) {
     
     // 先在缓存中查找是否存在
-    auto it = getTexture(x, y);
+    auto it = getTexture(rect, color);
     if(it) {
         return it;
     }
@@ -53,37 +53,44 @@ SDL_Texture* TextureManager::createTextureFromRect(int x, int y, SDL_FRect& rect
     SDL_FRect renderRect = {0, 0, rect.w, rect.h};
     SDL_RenderFillRect(m_renderer, &renderRect);
     // 缓存
-    m_cacheTexture.emplace(makeHash(x, y), newTexture);
+    m_cacheTexture.emplace(makeHash(rect, color), newTexture);
 
     // 恢复渲染目标
     SDL_SetRenderTarget(m_renderer, currentTexture);
     return newTexture;
 }
 
-size_t TextureManager::makeHash(int x, int y) {
+size_t TextureManager::makeHash(SDL_FRect& rect, SDL_Color& color) {
     // 分别计算字体名称和大小的哈希值
-    size_t h1 = std::hash<int>{}(x);
-    size_t h2 = std::hash<int>{}(y);
+    size_t h1 = std::hash<float>{}(rect.x);
+    size_t h2 = std::hash<float>{}(rect.y);
+    size_t h3 = std::hash<float>{}(rect.w);
+    size_t h4 = std::hash<float>{}(rect.h);
+
+    size_t h5 = std::hash<Uint8>{}(color.r);
+    size_t h6 = std::hash<Uint8>{}(color.g);
+    size_t h7 = std::hash<Uint8>{}(color.b);
+    size_t h8 = std::hash<Uint8>{}(color.a);
 
     // 组合两个哈希值：使用XOR运算和位左移组合两个独立的哈希值
     // 这样可以确保不同的fontID或ptSize组合都会产生不同的哈希值
-    return h1 ^ (h2 << 1);
+    return h1 ^ (h2 << 1) ^ (h3 << 2) ^ (h4 << 3) ^ (h5 << 4) ^ (h6 << 5) ^ (h7 << 6) ^ (h8 << 7);
 }
 
-SDL_Texture* TextureManager::getTexture(int x, int y) {
-    auto key = makeHash(x, y);
+SDL_Texture* TextureManager::getTexture(SDL_FRect& rect, SDL_Color& color) {
+    auto key = makeHash(rect, color);
     auto it = m_cacheTexture.find(key);
 
     if (it != m_cacheTexture.end()) {
         return it->second;
     } else {
-        SDL_Log("TextureManager: texture %d %d not exist!", x, y);
+        SDL_Log("TextureManager: texture not exist!");
         return nullptr;
     }
 }
 
-bool TextureManager::destoryTexture(int x, int y) {
-    int key = makeHash(x, y);
+bool TextureManager::destoryTexture(SDL_FRect& rect, SDL_Color& color) {
+    int key = makeHash(rect, color);
     auto it = m_cacheTexture.find(key);
     if (it == m_cacheTexture.end()) {
         SDL_Log("can't find the texture\n");
