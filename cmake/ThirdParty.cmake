12d3df55f22599bd0dd0b4e21ccc863ee562b105
zhenyan121 <104683324+zhenyan121@users.noreply.github.com>
2026-02-06 13:09:48 +0800

    build: add glm library

--- cmake/ThirdParty.cmake
+++ cmake/ThirdParty.cmake
@@ -14,3 +14,12 @@ FetchContent_Declare(
     GIT_TAG asio-1-36-0 # 指定一个稳定版本标签
 )
 FetchContent_MakeAvailable(asio)
+
+FetchContent_Declare(
+    glm
+    GIT_REPOSITORY https://github.com/g-truc/glm.git
+    GIT_TAG 1.0.3  # 使用稳定版本
+    # 或者使用最新的稳定版本标签，例如：
+    # GIT_TAG 1.0.1  # 请查看glm的发布页面获取最新稳定版本
+)
+FetchContent_MakeAvailable(glm)
